5a526e6a487414d4ec3ec8c4ff85f9be8cd72f96
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-03-22 06:20:34 -0500

    Se coloco la restriccion en la API

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
-router.get('/api/all/Users', /* auth.authenticate(), */ userController.getAllUsers);
-router.get('/api/id/Users/:id', /* auth.authenticate(),  */userController.getUserById);
-router.patch('/api/patch/Users/:id', /* auth.authenticate(),  */userController.updateUser);
+router.get('/api/all/Users', auth.authenticate(), userController.getAllUsers);
+router.get('/api/id/Users/:id', auth.authenticate(), userController.getUserById);
+router.patch('/api/patch/Users/:id', auth.authenticate(), userController.updateUser);
 router.post('/api/create/user', userController.createUser);
 router.delete('/api/delete/user/:id', userController.deleteUser);
 router.post('/register', userController.register);
